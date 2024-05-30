f3683da475db36b85e726d4c5ee884141714cb1a
rubenevivancos <rubenevivancos@hotmail.com>
2024-05-29 20:44:52 -0500

    Cambiando variables de entorno de la BD en aiven.io

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const port = process.env.PORT; //Esto es importantisimo, ya que Railway usa la variable PORT
+const port = process.env.PORT; //Esto es importantisimo, ya que Vercel usa la variable PORT
 
 
 conn.sync().then(() => {
